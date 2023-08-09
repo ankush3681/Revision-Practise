71c91db72830c91ea41452e7b6b557627251d794
ankush3681 <ankushkumar83623@gmail.com>
2023-08-09 21:06:40 +0530

    framing done

---
+++ dsa/practise.js
@@ -0,0 +1,6 @@
+// here we had to find the k number which is repeating most times in the array but in sorted format.
+
+let array = [2,2,3,4,4,1,1,1,4,5,4,2,6,2,5,4,4];    
+let k=2;         //output 2 4
+// if k=1    output 4
+// if k = 4   1 2 4 5
