dcdd5d130653b81ef45b136ace6d5927bc759aa6
ankush3681 <ankushkumar83623@gmail.com>
2023-08-09 23:58:16 +0530

    question framed

---
+++ dsa/practise2.js
@@ -0,0 +1,14 @@
+// here we had to count the total number of characters present between two "a", 
+// and if more than two "a" are present than we had to find the minimum total 
+// count of substring between two "a", both "a" will also be included.if less than two "a"
+// are present than print -1;
+
+let str = abcad; //ans = 4;
+// let str =  abcabdaadg  ans = 2;
+// let str = algh ans = -1  // qki "a" do bar ni aaya h
+
+
+// solution 
+
+
+
