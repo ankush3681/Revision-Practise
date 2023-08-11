c88fa2b246bd4df8c9ee544fbb9172c9aad3c87b
ankush3681 <ankushkumar83623@gmail.com>
2023-08-11 23:56:51 +0530

    hg

--- dsa/practise2.js
+++ dsa/practise2.js
@@ -1,5 +1,5 @@
-// here we had to count the total number of characters present between two "a", 
-// and if more than two "a" are present than we had to find the minimum total 
+// here we had to count the total number of characters present between two "a",
+// and if more than two "a" are present than we had to find the minimum total
 // count of substring between two "a", both "a" will also be included.if less than two "a"
 // are present than print -1;
 
@@ -7,8 +7,8 @@ let str = abcad; //ans = 4;
 // let str =  abcabdaadg  ans = 2;
 // let str = algh ans = -1  // qki "a" do bar ni aaya h
 
+// solution
 
-// solution 
-
-
-
+// The flow of Redux follows a unidirectional data flow pattern,
+//  which means that data in a Redux application flows in a single
+//  direction through a series of well-defined steps. The flow of Redux can be summarized as follows:
