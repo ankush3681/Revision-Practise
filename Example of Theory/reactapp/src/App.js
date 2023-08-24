558ed15afea741d57fdaac4bba0778bf1036c29a
ankush3681 <ankushkumar83623@gmail.com>
2023-08-24 21:20:41 +0530

    fragment example

--- Example of Theory/reactapp/src/App.js
+++ Example of Theory/reactapp/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Counter1 from './components/Counter1';
 import UseRef from './components/UseRef';
 import Counter2 from './components/WithoutJsx';
+import FragmentExample from './components/Fragment';
 
 function App() {
   return (
     <div className="App">
       {/* <Counter1/> */}
       {/* <UseRef/> */}
-      <Counter2/>
+      {/* <Counter2/> */}
+      <FragmentExample/>
     </div>
   );
 }
---
+++ Example of Theory/reactapp/src/components/Fragment.jsx
@@ -0,0 +1,12 @@
+
+import React,{Fragment} from 'react';
+
+function FragmentExample() {
+  return (
+    <Fragment>
+      <h1>Hello</h1>
+      <p>This is a React fragment example.</p>
+    </Fragment>
+  );
+}
+export default FragmentExample;
