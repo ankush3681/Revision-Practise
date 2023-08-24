8ffe463cff450617649e54bafc4d5fcc4bff3948
ankush3681 <ankushkumar83623@gmail.com>
2023-08-24 14:42:26 +0530

    useReducer

---
+++ Example of Theory/reactapp/src/components/Counter1.jsx
@@ -0,0 +1,39 @@
+
+
+// useReducer Example
+
+
+import React, { useReducer } from 'react';
+
+const initialState = { count: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+}
+
+export default function Counter1() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const increment = () => {
+    dispatch({ type: 'increment' });
+  };
+
+  const decrement = () => {
+    dispatch({ type: 'decrement' });
+  };
+
+  return (
+    <div>
+      <p>Count: {state.count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+}
